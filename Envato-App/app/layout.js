d50d0a1f6f2c67e93c089fa173519bd99b206145
L037039 <b_shashank@lilly.com>
2025-05-28 09:56:19 +0530

    Home component

--- Envato-App/app/layout.js
+++ Envato-App/app/layout.js
@@ -1,12 +1,17 @@
 import "./globals.css";
 
-import Header from "@/components/Header/MainHeader";
+import Header from "@/components/Header/MainHeader/MainHeader";
+import Home from "@/components/Home/Home";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link href='https://cdn.boxicons.com/fonts/basic/boxicons.min.css' rel='stylesheet'/>
+      </head>
       <body>
         <Header/>
+        <Home/>
         {children}
       </body>
     </html>
---
+++ Envato-App/components/Home/Home.jsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    // className="bg-[url('/doctorBackground.jpg')] bg-cover h-screen"
+    <div className="bg-gradient-to-r from-blue-400 to-white h-140 flex items-center">
+      <div className="w-full md:w-1/2 px-6 md:pl-12 pl-4">
+      <h1 className="md:text-5xl text-[#010304cf] font-bold text-start font-serif text-4xl break-words">
+        Best Choice For Medical Healthcare
+      </h1>
+      <p className="mt-4 text-gray-700 md:text-2xl break-words text-start">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
+      </div>
+    </div>
+  );
+}
