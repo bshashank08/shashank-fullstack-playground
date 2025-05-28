c7a4bd9ea889ea50bf8862da63fe21d6a482aa71
L037039 <b_shashank@lilly.com>
2025-05-28 09:49:10 +0530

    Moved menuItems to a separate file

--- Envato-App/components/Header/Menu/Menu.jsx
+++ Envato-App/components/Header/Menu/Menu.jsx
@@ -1,71 +1,51 @@
+import menuItems from "../../../utils/menuPages";
+import companyLogo from "../../../public/companyLogo.svg";
+
 export default function Menu() {
-    const menuItems = [
-        { name: "HOME", href: "/"},
-        {name: "SERVICES",
-            subPages : [
-                {
-                    name: "Consultation", href: "/services/consultation"
-                },
-                { name: "Surgery", href: "/services/surgery"},
-                { name: "Therapy", href: "/services/therapy"},
-                { name: "Diagnosis", href: "/services/diagnosis"},
-                { name: "Emergency", href: "/services/emergency"},
-            ]
-        },
-        {name: "PAGES",
-            subPages : [
-                {name: "About", href: "/about"},
-                {name: "Doctors", href: "/doctors"},
-                {name: "Blog", href: "/blog"},
-            ]
-        },
-        {name: "CONTACT" ,
-            subPages: [
-                {name: "ContactStyle 1", href: "/contact-style-1"},
-                {name: "ContactStyle 2", href: "/contact-style-2"},
-                {name: "ContactStyle 3", href: "/contact-style-3"},
-            ]
-        },
-        {name: "BLOG", href: "/blog"},
-    ]
+  return (
+    <>
+      <div className="sticky top-14 bg-white h-18 ">
+        <header className="hidden md:flex justify-around items-center-safe h-18 px-8">
+          <div className="flex justify-between items-center ">
+            <img src="./companyLogo.svg" alt="T" className="h-full w-20 hover:scale-105"/>
+          </div>
+          <nav className=" flex text-black space-x-4 ">
+            {menuItems.map((item, index) => (
+              <div key={index} className="relative group">
+                <a href={item.href || "#"} className="hover:text-[#00a3c8]">
+                  {item.name}
+                </a>
+                {item.subPages && (
+                  <div className=" absolute left-0 bg-white hidden group-hover:block ">
+                    <ul>
+                      {item.subPages.map((subItem, subIndex) => (
+                        <li
+                          className=" hover:bg-[#00a3c8] hover:text-white w-fit py-0.5"
+                          key={subIndex}
+                        >
+                          <a href={subItem.href}>{subItem.name}</a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
+              </div>
+            ))}
+          </nav>
 
-    return(
-        <>
-           <div className="bg-white h-20 flex justify-between items-center-safe px-20">
-           <div className= "flex justify-between items-center ">
-                <img src="https://www.thetahmid.com/themes/mediseba-v1.2/images/logo.png" alt="T"></img>
-            </div>
-            <nav className=" flex text-black space-x-4 ">
-            {menuItems.map((item, index)=> (
-                <div key={index} className="relative group" >
-                    <a href={item.href || "#"} className="hover:text-[#00a3c8]">{item.name}</a>
-                    {item.subPages && (
-                        <div className=" absolute left-0 bg-white hidden group-hover:block ">
-                            <ul >
-                                {item.subPages.map((subItem, subIndex) =>(
-                                    <li className=" hover:bg-[#00a3c8] hover:text-white border-b-[#eeeeee9f] w-fit py-0.5" key={subIndex}>
-                                    <a href={subItem.href}>{subItem.name}</a></li>
-                                ))}
-                            </ul>
-                        </div>
-                    )}
-                </div>
-            )
-            )}
-            </nav>
-
-            {/*search bar*/}
-            <div>
-                <input type="text" placeholder="Search"/>
-                <button
-                        type="submit"
-                        className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-[#00a3c8]"
-                    >
-                    </button>
-            </div>
-           </div>
-            
-            
-        </>
-    )
+          {/* Search Bar */}
+          <div className="relative hidden md:flex items-center justify-center gap-3">
+            <input
+                type="text"
+                placeholder="Search"
+                className="border border-gray-300 text-black rounded-4xl px-4 py-2 focus:outline-none focus:ring-2 focus:ring-gray-500"
+            />
+            <i className="absolute right-3 text-2xl bx bx-search text-[#382b2b6d]"></i>
+          </div>
+          {/* Hamburger Menu Icon */}
+          <i className="bx bx-menu text-black md:hidden xl:hidden block cursor-pointer text-3xl"></i>
+        </header>
+      </div>
+    </>
+  );
 }
---
+++ Envato-App/utils/menuItems.js
@@ -0,0 +1,30 @@
+const menuItems = [
+    { name: "HOME", href: "/"},
+    {name: "SERVICES",
+        subPages : [
+            {
+                name: "Consultation", href: "/services/consultation"
+            },
+            { name: "Surgery", href: "/services/surgery"},
+            { name: "Therapy", href: "/services/therapy"},
+            { name: "Diagnosis", href: "/services/diagnosis"},
+            { name: "Emergency", href: "/services/emergency"},
+        ]
+    },
+    {name: "PAGES",
+        subPages : [
+            {name: "About", href: "/about"},
+            {name: "Doctors", href: "/doctors"},
+            {name: "Blog", href: "/blog"},
+        ]
+    },
+    {name: "CONTACT" ,
+        subPages: [
+            {name: "ContactStyle 1", href: "/contact-style-1"},
+            {name: "ContactStyle 2", href: "/contact-style-2"},
+            {name: "ContactStyle 3", href: "/contact-style-3"},
+        ]
+    }
+]
+
+export default menuItems;
