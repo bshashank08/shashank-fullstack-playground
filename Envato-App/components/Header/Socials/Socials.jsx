a817e90d9320c0562291efd5a42441cbe700e3e2
L037039 <b_shashank@lilly.com>
2025-06-13 11:11:06 +0530

    Header- social media component

--- Envato-App/components/Header/Socials/Socials.jsx
+++ Envato-App/components/Header/Socials/Socials.jsx
@@ -1,12 +1,12 @@
 import Contact from "./Contact";
 import Icons from "../../../assets/Icons";
-import Tumblr from "../../../public/tumblr.svg";
+import Tumblr from "@/public/tumblr.svg";
+import Link from "next/link";
 
 export default function Socials() {
   return (
-    <div className="bg-white flex justify-between h-11 items-center px-60 text-black border-b-[#eeee] border-b-1 ">
-      <div>
-        <div className="flex gap-8">
+    <div className="hidden xl:flex justify-around items-center h-11 bg-white text-black border-b-[#eeee] border-b-1 ">
+        <div className="flex flex-nowrap items-center justify-between gap-8">
           <div>
             <Contact icon={Icons.Phone}>+1 (593) 948 6453 </Contact>
           </div>
@@ -17,24 +17,25 @@ export default function Socials() {
             <Contact icon={Icons.Email}>enquery@medino.com</Contact>
           </div>
         </div>
-      </div>
       <div className="flex items-center justify-around space-x-5">
       <div className="flex space-x-4">
-      <div>{Icons.Twitter}</div>
+      <div className="hover:scale-140">{Icons.Twitter}</div>
       <div>
-        <a href="google.com"><img src="/tumblr.svg" className="h-3 w-3 mt-0.5 " alt="T" /></a> 
+        <a href="google.com"><img src="/tumblr.svg" className="h-3 w-3 mt-0.5 hover:scale-140" alt="T" /></a> 
       </div>
       <div>
-        <img src="./youtube.svg" className="h-4 w-4 " />
+        <img src="./youtube.svg" className="h-4 w-4 hover:scale-140" />
       </div>
       <div>
-        <img src="./tik-tok.svg" className="h-4 w-4 " />
+        <img src="./tik-tok.svg" className="h-4 w-4 hover:scale-140" />
       </div>
       </div>
       <div>
-        <button className="bg-[#00a3c8] h-9 rounded-3xl text-white text-sm w-44">
+        <Link href="/Appointments">
+        <button href="/Appointments" className="bg-[#00a3c8] h-9 rounded-3xl text-white text-sm w-44 hover:bg-[#6ebacb] cursor-pointer">
           Make an Appointment
         </button>
+        </Link>
       </div>
       </div>
     </div>
