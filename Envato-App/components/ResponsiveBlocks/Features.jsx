d9ab99da2bd215149f47d53cf58813bad6f1c6da
L037039 <b_shashank@lilly.com>
2025-06-18 00:18:32 +0530

    Increased responsive blocks image size

--- Envato-App/components/ResponsiveBlocks/Features.jsx
+++ Envato-App/components/ResponsiveBlocks/Features.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function ResponsiveBlocks({svg, children}) {
     return (
         <div className='flex flex-col justify-evenly items-center h-full p-10'>
-            <Image width={30} height={30} alt="Image" src={svg}/>
+            <Image width={40} height={40} alt="Image" src={svg}/>
             {children}
         </div>
     )
