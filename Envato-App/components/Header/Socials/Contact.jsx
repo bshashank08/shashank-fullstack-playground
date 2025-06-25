8049839f0c28abb1570ff1a3db4c436423bd7dd8
L037039 <b_shashank@lilly.com>
2025-06-25 15:02:42 +0530

    deleted icons.js and added separate svg files

--- Envato-App/components/Header/Socials/Contact.jsx
+++ Envato-App/components/Header/Socials/Contact.jsx
@@ -1,7 +1,9 @@
-export default function Contact({icon, children}) {
+import Image from 'next/image';
+
+export default function Contact({icon, altText, children}) {
     return (
         <div className="flex items-center space-x-2">
-            <span className="" >{icon}</span>
+            <span className="" ><Image src={icon} alt={altText} width={20} height={20}/></span>
             <span className="text-[#666666] text-sm font-light font-  ">{children}</span>
         </div>
     )
--- Envato-App/components/Header/Socials/Socials.jsx
+++ Envato-App/components/Header/Socials/Socials.jsx
@@ -1,15 +1,21 @@
 "use client";
 
 import Contact from "./Contact";
-import Icons from "../../../public/assets/Icons";
 import Image from "next/image";
 import Tumblr from "@/public/assets/tumblr.svg";
 import Youtube from "@/public/assets/youtube.svg";
 import Tiktok from "@/public/assets/tik-tok.svg";
+import Phone from "@/public/assets/phone.svg";
+import headerClock from "@/public/assets/headerClock.svg";
+import Email from "@/public/assets/email.svg";
+import Twitter from "@/public/assets/twitter.svg";
 import { useState } from "react";
 import AppointmentModalWrapper from "../../Appointment/AppointmentWrapper";
+import { useUser } from "@auth0/nextjs-auth0";
+import Link from "next/link";
 
 export default function Socials() {
+  const { user, isLoading } = useUser();
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleOpenModal = () => {
@@ -25,21 +31,23 @@ export default function Socials() {
       <div className="hidden xl:flex justify-around items-center h-11 bg-white text-black border-b-[#eeee] border-b-1 ">
         <div className="flex flex-nowrap items-center justify-between gap-8">
           <div>
-            <Contact icon={Icons.Phone}>+1 (593) 948 6453 </Contact>
+            <Contact icon="/assets/phone.svg" altText="Phone" >+1 (593) 948 6453 </Contact>
           </div>
           <div>
-            <Contact icon={Icons.Clock}>Mon - Sun 7:30am - 9:30pm</Contact>
+            <Contact icon="/assets/headerClock.svg" altText="Clock">Mon - Sun 7:30am - 9:30pm</Contact>
           </div>
           <div>
-            <Contact icon={Icons.Email}>enquery@medino.com</Contact>
+            <Contact icon="/assets/email.svg" altText="Email">enquery@medino.com</Contact>
           </div>
         </div>
         <div className="flex items-center justify-around space-x-5">
           <div className="flex space-x-4">
             <div className="hover:scale-140">
-              <a href="google.com">
-               {Icons.Twitter}
-              </a>
+              <Image
+                  src={Twitter}
+                  className="h-4 w-4 mt-0.5 mb-0.5 hover:scale-140"
+                  alt="T"
+                />
             </div>
             <div>
               <a href="google.com">
@@ -52,12 +60,20 @@ export default function Socials() {
             </div>
             <div>
               <a href="google.com">
-                <Image src={Youtube} className="h-4 w-4 hover:scale-140" alt="Y" />
+                <Image
+                  src={Youtube}
+                  className="h-4 w-4 hover:scale-140"
+                  alt="Y"
+                />
               </a>
             </div>
             <div>
               <a href="google.com">
-                <Image src={Tiktok} className="h-3.5 w-4 hover:scale-140 " alt="TK"/>
+                <Image
+                  src={Tiktok}
+                  className="h-3.5 w-4 hover:scale-140 "
+                  alt="TK"
+                />
               </a>
             </div>
           </div>
@@ -74,6 +90,18 @@ export default function Socials() {
             )}
             {/* </Link> */}
           </div>
+          {user && (
+            <div>
+              <Link href="/Profile">
+                <img
+                  src={user.picture}
+                  alt="Profile"
+                  className="rounded-full border-black border-[1] h-9 w-9"
+                  decode="async"
+                />
+              </Link>
+            </div>
+          )}
         </div>
       </div>
       {}
