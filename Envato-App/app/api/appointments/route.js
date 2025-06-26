b138a232050f61dc2139a0833d4ac166705136ea
L037039 <b_shashank@lilly.com>
2025-06-26 20:53:53 +0530

    Included DB pool in a separate file

--- Envato-App/app/api/appointments/route.js
+++ Envato-App/app/api/appointments/route.js
@@ -1,19 +1,10 @@
-// import { query } from '../../../lib/dbConnect.js'
+import { query } from '../../../lib/dbConnect.js'
 import { NextResponse } from "next/server";
-import { Pool } from "pg";
-
-export const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'ENVATO-APP',
-  password: 'incorrect@psql',
-  port: 5433,
-});
 
 export async function GET() {
     try {
         console.log('Fetching appointment details...');
-        const result = await pool.query('SELECT id, name, email, phone_number, appointment_date FROM appointments');
+        const result = await query('SELECT id, name, email, phone_number, appointment_date FROM appointments');
         return NextResponse.json({appointments: result.rows}, {status: 200});
     } catch (error) {
         console.error('Error fetching users:', error);
@@ -24,7 +15,7 @@ export async function GET() {
 export async function POST(req) {
     try {
         const { name, email, phone_number, appointment_date } = await req.json();
-        const result = await pool.query(
+        const result = await query(
             'INSERT INTO appointments (name, email, phone_number, appointment_date) VALUES ($1, $2, $3, $4) RETURNING *',
             [name, email, phone_number, appointment_date]
         );
--- Envato-App/lib/dbConnect.js
+++ Envato-App/lib/dbConnect.js
@@ -1,4 +1,3 @@
-// import { NextResponse } from "next/server";
 import { Pool } from "pg";
 
 // connection pool
@@ -10,32 +9,4 @@ export const pool = new Pool({
   port: 5433,
 });
 
-// export default async function dbConnect(){
-//     await pool.connect((err, client, release) =>{
-//         if (err) {
-//             return console.error('Error acquiring client', err.stack);
-//             // return NextResponse.json({ error: 'Database connection failed' }, { status: 500 });
-//         }
-//         client.query('SELECT NOW()', (err, result) => {
-//             release();
-//             if (err) {
-//                 return console.error('Error executing query', err.stack);
-//                 // return NextResponse.json({ error: 'Query execution failed' }, { status: 500 });
-//             }
-//             console.log('Database connected successfully:', result.rows[0]);
-//         });
-//     })
-// }
-
-// export default async function dbConnect() {
-//   try {
-//     const client = await pool.connect();
-//     return client;
-//   } catch (err) {
-//     console.error("Database connection error:", err.stack);
-//     throw err;
-//   }
-// }
-
-// Exporting a query function for reuse
 export const query = (text, params) => pool.query(text, params);
