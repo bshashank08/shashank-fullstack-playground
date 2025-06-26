d7d59d4df1033a18eda55156e10672ae9bfe1b03
L037039 <b_shashank@lilly.com>
2025-06-26 22:58:43 +0530

    removed comments

--- Envato-App/lib/auth0.js
+++ Envato-App/lib/auth0.js
@@ -1,11 +1,6 @@
-// lib/auth0.js
-
 import { Auth0Client } from "@auth0/nextjs-auth0/server";
 
-// Initialize the Auth0 client 
 export const auth0 = new Auth0Client({
-  // Options are loaded from environment variables by default
-  // Ensure necessary environment variables are properly set
   domain: process.env.AUTH0_DOMAIN,
   clientId: process.env.AUTH0_CLIENT_ID,
   clientSecret: process.env.AUTH0_CLIENT_SECRET,
--- Envato-App/lib/dbConnect.js
+++ Envato-App/lib/dbConnect.js
@@ -1,6 +1,5 @@
 import { Pool } from "pg";
 
-// connection pool
 export const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
